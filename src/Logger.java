b9f9a4ac011801b2825b7539ed3786a9e02c7288
Drov3r <65616524+Drov3r@users.noreply.github.com>
2021-03-14 22:16:21 -0400

    Update Logger.java

--- src/Logger.java
+++ src/Logger.java
@@ -14,7 +14,7 @@ public class Logger {
     static boolean[] change=new boolean[7];
 
     //static String number = "76561198176055490";
-    static  String [] Players={"76561198176055490","76561198275589508","76561198148742258","76561198271159103"};
+    static  String [] Players={"76561198176055490","76561198275589508","76561198148742258","76561198271159103","76561198288140418"};
     public static void main(String[] args ) throws IOException {
 
         //while (true) {
