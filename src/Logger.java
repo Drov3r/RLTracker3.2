e379d021fb3e9a7458ebc4f18294d137681d0c69
gunsmoke1416 <57071455+gunsmoke1416@users.noreply.github.com>
2021-07-23 22:21:31 -0400

    3.2.1

--- src/Logger.java
+++ src/Logger.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -14,169 +15,61 @@ public class Logger {
     static boolean[] change=new boolean[7];
 
     //static String number = "76561198176055490";
-    static  String [] Players={"76561198176055490","76561198275589508","76561198148742258","76561198271159103","76561198288140418"};
+    static  String [] Players;
+    static  String [] Platform;
+    static String[] playlistNames = {"Solo", "Twos", "Threes", "Tournaments"};
     public static void main(String[] args ) throws IOException {
-
-        //while (true) {
-        for(String number:Players) {
-            int[] playlist = {10, 11, 13, 27, 28, 29, 34};
-            String[] playlistNames = {"Solo", "Twos", "Threes", "Hoops", "Rumble", "DropShot", "Tournaments"};
-
-            for (boolean x : change) x = false;
-            //"76561198176055490"   mine
-
-
-            int[] current = new int[7];
-            int i = 0;
-            int j = 0;
-            writeFile(number, playlistNames);
-            String PATH = number + "/";
-
-            for (String x : playlistNames) {
-                int num;
-                BufferedReader reader;
-                try {
-                    String fileName = x + ".txt";
-                    reader = new BufferedReader(new FileReader(PATH + "/" + fileName));
-                    String line = reader.readLine();
-                    String l = line;
-                    while (line != null) {
-                        //System.out.println(line);
-                        // read next line
-                        line = reader.readLine();
-                        if (line != null) {
-                            l = line;
-
-                        }
-                    }
-                    //System.out.println(playlistNames[j]);
-                    if (l.contains(",")) {
-                        num = removeComma(l);
-                    } else num = Integer.parseInt(l);
-                    current[j] = num;
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                j++;
+        String [][] records=new String[5][2];
+        int c=0;
+        try (BufferedReader br = new BufferedReader(new FileReader("check.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records[c]=values;
+                //System.out.println(values);
+                c++;
             }
+        }
+        Players=new String[records.length];
+        Platform=new String[records.length];
+        for (int i = 0; i < records.length; i++) {
+                Players[i]=records[i][0];
+                Platform[i]=records[i][1];
 
-
-            for (int x : playlist) {
-                Tracker mmr = new Tracker(x, number);
-                String rank = mmr.getMmr();
-                int num;
-                if (rank.contains(",")) {
-                    num = removeComma(rank);
-                } else num = Integer.parseInt(rank);
-
-                if (num != current[i]) {
-                    change[i] = true;
-                    try (FileWriter fw = new FileWriter(PATH + playlistNames[i] + ".txt", true);
-                         BufferedWriter bw = new BufferedWriter(fw);
-                         PrintWriter out = new PrintWriter(bw)) {
-                        out.println(num);
-                        current[i] = removeComma(rank);
-
-                    } catch (IOException e) {
-                        System.out.println(e);
-                    }
-
-                    try (FileWriter fw = new FileWriter(PATH + playlistNames[i] + "time.txt", true);
-                         BufferedWriter bw = new BufferedWriter(fw);
-                         PrintWriter out = new PrintWriter(bw)) {
-                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-                        LocalDateTime now = LocalDateTime.now();
-                        System.out.println(dtf.format(now));
-
-                        out.println(dtf.format(now));
-
-                    } catch (IOException e) {
-                        //exception handling left as an exercise for the reader
-                    }
-                }
-                i++;
-
-
+        }
+        c=0;
+        for(String number :Players) {
+            int[] mmr = Tracker.Tracker(number,Platform[c]);
+            System.out.println(Arrays.toString(mmr));
+            for (int i = 0; i < mmr.length; i++) {
+                writeFile(number,mmr[i],i);
             }
-//            try {
-//                makeGraph(number, playlistNames);
-//                System.out.println("Sleeping");
-//                TimeUnit.MINUTES.sleep(4);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            //while true }
+            c++;
         }
     }
 
-    private static void makeGraph(String number,String[] pN ) {
-        List<Double> list=new ArrayList<Double>();
-        for (int i = 0; i <change.length; i++) {
-            if(change[i]){
-                current=i;
-                change[i]=false;
-            }
-        }
-        String PATH = number + "/";
 
-        BufferedReader reader;
-        try {
-            String fileName = pN[current] + ".txt";
-            reader = new BufferedReader(new FileReader(PATH + "/" + fileName));
-            String line = reader.readLine();
-            String l = line;
-            while (line != null) {
-                //System.out.println(line);
-                // read next line
-                list.add(Double.parseDouble(line));
-                line = reader.readLine();
-
-
-            }
-
-
-        }catch (IOException e){
-            System.out.println(e);
-        }
-    }
-
-    public static int removeComma(String rank){
-        char []t=rank.toCharArray();
-        char []r=new char[t.length-1];
-        boolean found=false;
-        for (int k = 0; k < t.length-1 ; k++) {
-            if (t[k]==','){
-                found=true;
-            }
-            if (found){
-                t[k]=t[k+1];
-            }
-            //t[t.length-1]= 48;
-
-            for (int m = 0; m <r.length ; m++) {
-                r[m]=t[m];
-
-            }
-        }
-        String l= new String(r);
-        return Integer.parseInt(l);
-    }
-    public static void writeFile(String number, String[] name) throws IOException {
+    public static void writeFile(String number,int mmr,int i) throws IOException {
         String PATH = number+"/";
         File directory = new File(PATH);
 
-        if (! directory.exists()){
+        if (!directory.exists()){
             directory.mkdir();
-            for(String x:name){
-                String fileName=x+".txt";
-                File file = new File(PATH  + fileName);
-                if(file.exists()){
+        }
+                String fileName=playlistNames[i]+".txt";
+                String filepath=PATH+fileName;
+                //System.out.println(filepath);
+                File file = new File(filepath);
+                if(!file.exists()){
                     file.createNewFile();
                 }else {
-                    System.out.println(fileName+"Exists");
+                    //System.out.println(fileName+"Exists");
                 }
-            }
+
+        FileWriter fr = new FileWriter(file, true);
+        //System.out.println(mmr);
+        fr.write(String.valueOf(mmr)+'\n');
+        fr.close();
+
         }
     }
-}
--- src/Tracker.java
+++ src/Tracker.java
@@ -6,64 +6,108 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class Tracker {
-    String mmr="null";
 
-    public Tracker(int x,String number) {
-        boolean printnext=false;
 
-        String site="https://rocketleague.tracker.network/rocket-league/profile/steam/"+number+"/mmr?playlist="+x;
+    public Tracker(String number) {
+    }
+
+    public static int[] Tracker(String number,String plat) {
+        int ones=0,twos=0,threes=0,tourny=0;
         try {
+            Document doc = Jsoup.connect("https://api.yannismate.de/rank/"+plat+"/"+number+"?playlists=ranked_1v1,ranked_2v2,ranked_3v3,TOURNAMENTS").get() ;
 
-            Document doc = Jsoup.connect(site).get() ;
 
-
-            Elements paragraphs = doc.select(".value");
+            Elements paragraphs = doc.select("body");
             for(Element p : paragraphs) {
-                // System.out.println(p.text());
-                if (printnext){
-                    printnext=false;
-                    mmr=p.text();
-                }
-                if (p.text().contains("Division")){
-                    printnext=true;
+                //System.out.println(p.text());
+                String str=p.toString();
+                char [] text=str.toCharArray();
+                int count=0;
+
+                boolean record=false;
+                for (int i = 0; i < text.length; i++) {
+                    int start,finish;
+                    if(text[i]=='('){
+                        start=i;
+                        for (int j = i; j < i+6; j++) {
+                            if(text[j]==')') {
+                                finish=j;
+                                if (count==0)ones=getMMR(text,start,finish);
+                                if (count==1)twos=getMMR(text,start,finish);
+                                if (count==2)threes=getMMR(text,start,finish);
+                                if (count==3)tourny=getMMR(text,start,finish);
+                                count++;
+                            }
+                        }
+                    }
                 }
             }
-            //System.out.println(doc.body());
+
+
+            //System.out.println(doc);
         }catch (IOException e){
             System.out.println(e);
         }
-        System.out.println(mmr);
-        //return mmr;
-    }
-
-    public String getMmr() {
+        int mmr[]= new int[4];
+        mmr[0]=ones;
+        mmr[1]=twos;
+        mmr[2]=threes;
+        mmr[3]=tourny;
         return mmr;
     }
 
 
+
+
     public static void main(String[] args ){
         boolean printnext=false;
-        String mmr="null";
 
         try {
-            Document doc = Jsoup.connect("https://rocketleague.tracker.network/rocket-league/profile/steam/76561198176055490/mmr?playlist=11").get() ;
+            Document doc = Jsoup.connect("https://api.yannismate.de/rank/steam/76561198176055490?playlists=ranked_1v1,ranked_2v2,ranked_3v3,TOURNAMENTS").get() ;
 
 
-            Elements paragraphs = doc.select(".value");
+            Elements paragraphs = doc.select("body");
             for(Element p : paragraphs) {
-                // System.out.println(p.text());
-                if (printnext){
-                    printnext=false;
-                    mmr=p.text();
-                }
-                if (p.text().contains("Division")){
-                    printnext=true;
+                System.out.println(p.text());
+                String str=p.toString();
+                char [] text=str.toCharArray();
+                int count=0;
+                int ones,twos,threes,tourny;
+                boolean record=false;
+                for (int i = 0; i < text.length; i++) {
+                    int start,finish;
+                    if(text[i]=='('){
+                        start=i;
+                        for (int j = i; j < i+6; j++) {
+                            if(text[j]==')') {
+                                finish=j;
+                                if (count==0)ones=getMMR(text,start,finish);
+                                if (count==1)twos=getMMR(text,start,finish);
+                                if (count==2)threes=getMMR(text,start,finish);
+                                if (count==3)tourny=getMMR(text,start,finish);
+                                count++;
+                            }
+                        }
+                    }
                 }
             }
-            //System.out.println(doc.body());
+
+
+            System.out.println(doc);
         }catch (IOException e){
             System.out.println(e);
         }
-        System.out.println(mmr);
+
+    }
+
+    private static int getMMR(char[] text, int start, int finish) {
+        int mmr;
+        char[] temp= new char[finish-start-1];
+        for (int i = 0; i < finish-start-1; i++) {
+            temp[i]=text[start+i+1];
+        }
+        mmr=Integer.parseInt(String.copyValueOf(temp));
+        //System.out.println(mmr);
+        return mmr;
     }
 }
